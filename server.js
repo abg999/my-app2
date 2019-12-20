c0a512c5ecddbbb73625c34d68e6d0fe6c12b9de
Ashish Gangaramani <ashishgangaramani@gmail.com>
2019-12-21 03:04:43 +0530

    setting static file as index.js in case if app is in production and its not hitting api/

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const app = express();
 
 
 //BodyParser middleware
 app.use(bodyParser.json());
-app.get('/',function(req,res){
+app.get('/api',function(req,res){
 	res.send('hello');
+});
+if(process.env.NODE_ENV === 'production') {
+	//set static folder
+	app.use(express.static('client/pages'));
 
-})
+	app.get('*',(req ,res)=>{
+		res.sendFile(path.resolve(__dirname, 'client', 'pages', 'index.js'));
+	});
+}
 
 const port = process.env.PORT || 5000;
 
